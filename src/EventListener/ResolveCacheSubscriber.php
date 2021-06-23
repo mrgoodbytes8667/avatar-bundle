c222f8b815aff549c00d354355fcb1729350ac24
MrGoodBytes <bytes@goodbytes.live>
2021-06-23 13:02:11 -0500

    Update to use a combined Event/Message handler

--- src/EventListener/ResolveCacheSubscriber.php
+++ src/EventListener/ResolveCacheSubscriber.php
@@ -5,15 +5,28 @@ namespace Bytes\AvatarBundle\EventListener;
 
 
 use Bytes\AvatarBundle\Message\ResolveCacheEvent;
-use Liip\ImagineBundle\Async\ResolveCache;
+use Liip\ImagineBundle\Imagine\Filter\FilterManager;
+use Liip\ImagineBundle\Service\FilterService;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
+use Symfony\Contracts\EventDispatcher\EventDispatcherInterface;
 
 /**
  * Class ResolveCacheSubscriber
  * @package Bytes\AvatarBundle\EventListener
  */
-class ResolveCacheSubscriber implements EventSubscriberInterface
+class ResolveCacheSubscriber implements MessageHandlerInterface, EventSubscriberInterface
 {
+    /**
+     * ResolveCacheSubscriber constructor.
+     * @param FilterManager $filterManager
+     * @param FilterService $filterService
+     * @param EventDispatcherInterface $dispatcher
+     */
+    public function __construct(private FilterManager $filterManager, private FilterService $filterService, private EventDispatcherInterface $dispatcher)
+    {
+    }
+
     /**
      * Returns an array of event names this subscriber wants to listen to.
      *
@@ -43,12 +56,31 @@ class ResolveCacheSubscriber implements EventSubscriberInterface
     }
 
     /**
+     * @param ResolveCacheEvent $message
+     */
+    public function __invoke(ResolveCacheEvent $message)
+    {
+        $this->dispatcher->dispatch($message);
+    }
+
+    /**
      * @param ResolveCacheEvent $event
      * @return ResolveCacheEvent
      */
     public function onResolveCache(ResolveCacheEvent $event): ResolveCacheEvent
     {
-        new ResolveCache($event->getPath(), $event->getFilters(), $event->isForce());
+        $filters = $event->getFilters() ?: array_keys($this->filterManager->getFilterConfiguration()->all());
+        $path = $event->getPath();
+        $results = [];
+        foreach ($filters as $filter) {
+            if ($event->isForce()) {
+                $this->filterService->bustCache($path, $filter);
+            }
+
+            $results[$filter] = $this->filterService->getUrlOfFilteredImage($path, $filter);
+        }
+
+        $event->setResults($results);
 
         return $event;
     }
--- src/Message/ResolveCacheEvent.php
+++ src/Message/ResolveCacheEvent.php
@@ -14,7 +14,10 @@ use Symfony\Contracts\EventDispatcher\Event;
  */
 class ResolveCacheEvent extends Event
 {
-    use ResolveCacheTrait;
+    /**
+     * @var array
+     */
+    private $results = [];
 
     /**
      * ResolveCacheEvent constructor.
@@ -24,12 +27,11 @@ class ResolveCacheEvent extends Event
      * @param string $command
      * @param DateTimeImmutable|null $createdAt
      */
-    public function __construct(string $path, ?array $filters = null, bool $force = false, string $command = Commands::RESOLVE_CACHE, ?DateTimeImmutable $createdAt = null)
+    public function __construct(private string $path, private ?array $filters = null, private bool $force = false, private string $command = Commands::RESOLVE_CACHE, private ?DateTimeImmutable $createdAt = null)
     {
         if (empty($createdAt)) {
             $this->createdAt = new DateTimeImmutable();
         }
-        $this->set($path, $filters, $force, $command);
     }
 
     /**
@@ -41,5 +43,113 @@ class ResolveCacheEvent extends Event
     public static function new(string $path, ?array $filters = null, bool $force = false): static
     {
         return new static($path, $filters, $force);
+    }
+
+    /**
+     * @return array
+     */
+    public function getResults(): array
+    {
+        return $this->results;
+    }
+
+    /**
+     * @param array $results
+     * @return $this
+     */
+    public function setResults(array $results): self
+    {
+        $this->results = $results;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPath(): string
+    {
+        return $this->path;
+    }
+
+    /**
+     * @param string $path
+     * @return $this
+     */
+    public function setPath(string $path): self
+    {
+        $this->path = $path;
+        return $this;
+    }
+
+    /**
+     * @return array|null
+     */
+    public function getFilters(): ?array
+    {
+        return $this->filters;
+    }
+
+    /**
+     * @param array|null $filters
+     * @return $this
+     */
+    public function setFilters(?array $filters): self
+    {
+        $this->filters = $filters;
+        return $this;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isForce(): bool
+    {
+        return $this->force;
+    }
+
+    /**
+     * @param bool $force
+     * @return $this
+     */
+    public function setForce(bool $force): self
+    {
+        $this->force = $force;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCommand(): string
+    {
+        return $this->command;
+    }
+
+    /**
+     * @param string $command
+     * @return $this
+     */
+    public function setCommand(string $command): self
+    {
+        $this->command = $command;
+        return $this;
+    }
+
+    /**
+     * @return DateTimeImmutable
+     */
+    public function getCreatedAt(): DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * @param DateTimeImmutable|null $createdAt
+     * @return $this
+     */
+    public function setCreatedAt(?DateTimeImmutable $createdAt = null): self
+    {
+        $this->createdAt = $createdAt ?? new DateTimeImmutable();
+        return $this;
     }
 }

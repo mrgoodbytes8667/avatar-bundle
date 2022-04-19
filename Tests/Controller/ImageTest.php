6f84e8662abce6c77317383d175ef4670e5e4bb2
MrGoodBytes <bytes@goodbytes.live>
2022-04-19 15:34:53 -0500

    Symfony 6.1 ImageTest Fix

--- Tests/Controller/ImageTest.php
+++ Tests/Controller/ImageTest.php
@@ -194,6 +194,22 @@ class ImageTest extends TestCase
     }
 
     /**
+     *
+     */
+    public function testGetImageAsPngInvalidFileSymfony61()
+    {
+        $this->expectWarning();
+        $this->expectWarningMessage('Data is not in a recognized format');
+        //$this->expectException(UnsupportedMediaTypeHttpException::class);
+        $cache = $this->getMockBuilder(AdapterInterface::class)->getMock();
+        $url = $this->getSampleImage('txt');
+        $client = new MockHttpClient(new MockResponse(file_get_contents($url)));
+        $image = $this->setupImage($cache, $client, false, false);
+
+        $image->getImageAsPngFromUrl($this->faker->imageUrl());
+    }
+
+    /**
      * @return Generator
      */
     public function provideSampleImages()
